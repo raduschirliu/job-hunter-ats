049ef409a16eec5ea6a70bd7d670b346e5557388
Joel Wong <joel.wong@ucalgary.ca>
2020-10-26 19:41:46 -0600

    Forgot to include new files

---
+++ cpsc-471-project/Models/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cpsc_471_project.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
