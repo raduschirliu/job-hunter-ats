5084ae4d0acb9f3d3f12edb27604847e584e2e1e
Radu Schirliu <radu.schirliu@gmail.com>
2020-12-03 10:45:09 -0700

    feat: Added authorization to applications routes

--- cpsc-471-project/Controllers/ApplicationsController.cs
+++ cpsc-471-project/Controllers/ApplicationsController.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.EntityFrameworkCore;
 
 using cpsc_471_project.Models;
+using Microsoft.AspNetCore.Authorization;
+using cpsc_471_project.Authentication;
 
 namespace cpsc_471_project.Controllers
 {
@@ -25,23 +26,66 @@ namespace cpsc_471_project.Controllers
             _context = context;
         }
 
-        // GET: api/Application
+        // GET: api/applications
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ApplicationDTO>>> GetApplications()
         {
-            var app = await _context.Applications.ToListAsync();
+            User user = await userManager.FindByNameAsync(User.Identity.Name);
+            IList<string> roles = await userManager.GetRolesAsync(user);
+            List<Application> applications = new List<Application>();
 
-            // NOTE: the select function here is not querying anything
-            // it is simply converting the values to another format
-            // i.e. the functional programming map function is named Select in C#
-            return app.Select(x => ApplicationToDTO(x)).ToList();
+            if (roles.Contains(UserRoles.Admin))
+            {
+                // Display all applications for admin
+                applications = await _context.Applications.ToListAsync();
+            }
+            else if (roles.Contains(UserRoles.Recruiter))
+            {
+                // TODO: ?? ? ?? ?? Likely return all applications to jobs they manage? ??????
+            }
+            else
+            {
+                // Display all of your own applications
+                var query = from app in _context.Applications
+                            join resume in _context.Resumes on app.ResumeId equals resume.ResumeId
+                            where resume.CandidateId == user.Id
+                            select app;
+
+                applications = await query.ToListAsync();
+            }
+
+            return applications.Select(app => ApplicationToDTO(app)).ToList();
         }
 
-        // GET: api/Application/5
+        // GET: api/applications/{id}
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<ApplicationDTO>> GetApplication(long id)
         {
-            var application = await _context.Applications.FindAsync(id);
+            User user = await userManager.FindByNameAsync(User.Identity.Name);
+            IList<string> roles = await userManager.GetRolesAsync(user);
+            Application application = null;
+
+            if (roles.Contains(UserRoles.Admin))
+            {
+                // Display all applications for admin
+                application = await _context.Applications.FindAsync(id);
+            }
+            else if (roles.Contains(UserRoles.Recruiter))
+            {
+                // TODO: ?? ? ?? ?? Likely allowed to see if if only it's for a job they manage ??????
+            }
+            else
+            {
+                // Display all of your own applications
+                var query = from app in _context.Applications
+                            join resume in _context.Resumes on app.ResumeId equals resume.ResumeId
+                            where resume.CandidateId == user.Id && app.ApplicationId == id
+                            select app;
+
+                application = await query.FirstOrDefaultAsync();
+            }
 
             if (application == null)
             {
@@ -51,11 +95,14 @@ namespace cpsc_471_project.Controllers
             return ApplicationToDTO(application);
         }
 
+        // PATCH: api/applications/{id}
+        [Authorize]
         [HttpPatch("{id}")]
         public async Task<IActionResult> PatchApplication(long id, ApplicationDTO appDTO)
         {
             Application app = DTOToApplication(appDTO);
             app.DateSubmitted = DateTime.Now;
+
             if (id != app.ApplicationId)
             {
                 return BadRequest();
@@ -82,31 +129,76 @@ namespace cpsc_471_project.Controllers
             return NoContent();
         }
 
+        // POST: api/applications
+        [Authorize]
         [HttpPost]
         public async Task<ActionResult<ApplicationDTO>> PostApplication(ApplicationDTO appDTO)
         {
+            User user = await userManager.FindByNameAsync(User.Identity.Name);
+
             Application app = DTOToApplication(appDTO);
             app.DateSubmitted = DateTime.Now;
+
+            Resume resume = null;
+            
+            if (await userManager.IsInRoleAsync(user, UserRoles.Admin))
+            {
+                resume = await _context.Resumes.Where(r => r.ResumeId == app.ResumeId).FirstOrDefaultAsync();
+            }
+            else
+            {
+                resume = await _context.Resumes.Where(r => r.CandidateId == user.Id && r.ResumeId == app.ResumeId).FirstOrDefaultAsync();
+            }
+
+            if (resume == null)
+            {
+                return NotFound("Resume does not exist");
+            }
+
             _context.Applications.Add(app);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetApplication", new { id = app.ApplicationId }, app);
         }
 
-        // DELETE: api/Application/5
+        // DELETE: api/applications/{id}
+        [Authorize]
         [HttpDelete("{id}")]
         public async Task<ActionResult<ApplicationDTO>> DeleteApplication(long id)
         {
-            var app = await _context.Applications.FindAsync(id);
-            if (app == null)
+            User user = await userManager.FindByNameAsync(User.Identity.Name);
+            IList<string> roles = await userManager.GetRolesAsync(user);
+            Application application = null;
+
+            if (roles.Contains(UserRoles.Admin))
+            {
+                // Admin can delete any application
+                application = await _context.Applications.FindAsync(id);
+            }
+            else if (roles.Contains(UserRoles.Recruiter))
+            {
+                // TODO: ?? ? ?? ?? Likely allowed to delete if only it's for a job they manage ??????
+            }
+            else
+            {
+                // Display all of your own applications
+                var query = from app in _context.Applications
+                            join resume in _context.Resumes on app.ResumeId equals resume.ResumeId
+                            where resume.CandidateId == user.Id && app.ApplicationId == id
+                            select app;
+
+                application = await query.FirstOrDefaultAsync();
+            }
+
+            if (application == null)
             {
                 return NotFound();
             }
 
-            _context.Applications.Remove(app);
+            _context.Applications.Remove(application);
             await _context.SaveChangesAsync();
 
-            return ApplicationToDTO(app);
+            return ApplicationToDTO(application);
         }
 
         private bool ApplicationExists(long id)
