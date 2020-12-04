c85ebfdba64c1c69e41f87b8ca98a0643bcaa948
Alexa Calkhoven <alexa.calkhoven1@ucalgary.ca>
2020-12-03 18:51:37 -0700

    Referrals again but on the right branch

---
+++ cpsc-471-project/Migrations/20201204014918_referrals.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cpsc_471_project.Migrations
+{
+    public partial class referrals : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Referrals",
+                columns: table => new
+                {
+                    ApplicationId = table.Column<long>(nullable: false),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    Email = table.Column<string>(maxLength: 255, nullable: true),
+                    Position = table.Column<string>(maxLength: 255, nullable: true),
+                    Company = table.Column<string>(maxLength: 255, nullable: true),
+                    Phone = table.Column<string>(maxLength: 15, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Referrals", x => new { x.ApplicationId, x.Name });
+                    table.ForeignKey(
+                        name: "FK_Referrals_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "ApplicationId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Referrals");
+        }
+    }
+}
