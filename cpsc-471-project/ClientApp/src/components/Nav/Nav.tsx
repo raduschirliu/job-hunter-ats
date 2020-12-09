436c7751f1fa97b374bfe00551b3d888f400cc08
Radu Schirliu <radu.schirliu@gmail.com>
2020-12-09 16:25:47 -0700

    fix: Navbar styling

--- cpsc-471-project/ClientApp/src/components/Nav/Nav.tsx
+++ cpsc-471-project/ClientApp/src/components/Nav/Nav.tsx
@@ -1,26 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../contexts/AuthContext';
 
 import './Nav.css';
 
 const Nav = () => {
-  const { isLoggedIn } = useContext(AuthContext);
+  const { getUser, isLoggedIn } = useContext(AuthContext);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    setName(getUser()?.firstName + ' ' + getUser()?.lastName);
+  }, [getUser]);
 
   return (
     <div className="nav-container">
       {isLoggedIn() ? (
         <>
-          <Link className="nav-container-link" to="/companies">Companies</Link>
-          <Link className="nav-container-link" to="/users">Users</Link>
-          <Link className="nav-container-link" to="/jobposts">Job Posts</Link>
-          <Link className="nav-container-link" to="/resumes">Resumes</Link>
-          <Link className="nav-container-link" to="/logout">Logout</Link>
+          <Link className="nav-container-link" to="/companies">
+            Companies
+          </Link>
+          <Link className="nav-container-link" to="/users">
+            Users
+          </Link>
+          <Link className="nav-container-link" to="/jobposts">
+            Job Posts
+          </Link>
+          <Link className="nav-container-link" to="/resumes">
+            Resumes
+          </Link>
+
+          <div className="nav-container-right">
+            <Link className="nav-container-link" to={`/users/${getUser()?.userName}`}>
+              {`Hi ${name}`}
+            </Link>
+            <Link className="nav-container-link" to="/logout">
+              Logout
+            </Link>
+          </div>
         </>
       ) : (
         <>
-          <Link to="/login" className="nav-container-link">Login</Link>
-          <Link to="/register" className="nav-container-link">Register</Link>
+          <Link to="/login" className="nav-container-link">
+            Login
+          </Link>
+          <Link to="/register" className="nav-container-link">
+            Register
+          </Link>
         </>
       )}
     </div>
